bbc56600261f8b46a9dc95697f97be929cd60560
Shaqeal Jinnah <j.shaqeal@gmail.com>
2024-09-15 22:46:14 +1000

    added npm packages to fix bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,69 +1,69 @@
 import React from "react";
-import { AiFillInstagram, AiFillTikTok } from "react-icons/ai";
+import { AiFillInstagram, AiFillTikTok, AiFillMail } from "react-icons/ai";
+import footerLogo from "../assets/final logo updated svg.svg";
 
 function Footer() {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <footer className="bg-[#bfbebb] text-white py-20">
-      <div className="container mx-auto px-6 flex justify-between">
-        <div className="max-w-20">
-          <img
-            className="w-full"
-            src="/favicon-cropped.svg"
-            alt="footer-logo"
-          />
+    <footer className="bg-[#bfbebb] py-16 text-white">
+      <div className="container mx-auto flex flex-col justify-between space-y-6 px-6 md:flex-row md:space-y-0 lg:max-w-[860px] xl:max-w-[1024px]">
+        <div className="mb-16">
+          <img className="w-44" src={footerLogo} alt="footer-logo" />
         </div>
-        <div className="flex-col space-y-2">
-          <div className="text-[#353333] font-bold">Quick Links</div>
-          <ul className="space-y-1 text-[#353333]">
-            <li className="hover:text-black duration-300">
+        <div className="flex-col space-y-4">
+          <div className="text-lg font-bold text-[#353333] xl:text-[20px]">Quick Links</div>
+          <ul className="space-y-1 text-[#353333] xl:text-[18px]">
+            <li className="duration-300 hover:text-black">
               <a href="/#">Home</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="#about">About</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="#services">Services</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="#contact">Contact</a>
             </li>
           </ul>
         </div>
-        <div className="flex-col space-y-2">
-          <div className="text-[#353333] font-bold">Services</div>
-          <ul className="space-y-1 text-[#353333]">
-            <li className="hover:text-black duration-300">
+        <div className="flex-col space-y-4">
+          <div className="text-lg font-bold text-[#353333] xl:text-[20px]">Services</div>
+          <ul className="space-y-1 text-[#353333] xl:text-[18px]">
+            <li className="duration-300 hover:text-black">
               <a href="/#services">Engagements</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="/#services">Birthdays</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="/#services">Bridal Party</a>
             </li>
-            <li className="hover:text-black duration-300">
+            <li className="duration-300 hover:text-black">
               <a href="/#services">Baby Shower</a>
             </li>
           </ul>
         </div>
-        <div className="space-y-2">
-          <div className="text-[#353333] font-bold">Our Socials</div>
-          <div className="flex justify-center items-center space-x-3">
+        <div className="space-y-4">
+          <div className="text-lg font-bold text-[#353333] xl:text-[20px]">Our Socials</div>
+          <div className="flex items-center justify-start space-x-3">
             <a href="https://www.instagram.com/salehievents/" target="_blank">
-              <AiFillInstagram className="text-gray-500 hover:text-black duration-300 h-9 w-9" />
+              <AiFillInstagram className="h-9 w-9 text-gray-500 duration-300 hover:text-black" />
             </a>
             <a href="https://www.tiktok.com/@salehievents" target="_blank">
-              <AiFillTikTok className="text-gray-500 h-9 w-9 hover:text-black" />
+              <AiFillTikTok className="h-9 w-9 text-gray-500 hover:text-black" />
+            </a>
+            <a href="https://www.tiktok.com/@salehievents" target="_blank">
+              <AiFillMail className="h-9 w-9 text-gray-500 hover:text-black" />
             </a>
           </div>
         </div>
       </div>
-      {/* <div className="flex justify-center text-black mt-4">
-        <p>&copy;{year}, Salehi Events. All rights reserved.</p>
-      </div> */}
+      <div className="mt-16 flex justify-center text-black lg:mt-24">
+        <p>&copy; {year}, Salehi Events. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,15 +1,15 @@
-import { Button } from '@mui/material';
-import bannerVideo from '../assets/banner-video.mp4'
+import { Button } from "@mui/material";
+import bannerVideo from "../assets/banner-video.mp4";
 
 function HeroSection() {
   return (
     <section
       id="home"
-      className="h-screen relative bg-blue-500 text-white flex items-center justify-center"
+      className="relative flex h-screen items-center justify-center bg-blue-500 text-white"
     >
       {/* Background video */}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute left-0 top-0 h-full w-full object-cover"
         src={bannerVideo}
         autoPlay
         loop
@@ -19,15 +19,20 @@ function HeroSection() {
 
       {/* Overlay content */}
       <div className="container relative z-10 mx-auto px-6 text-center">
-        <h2 className="text-4xl font-semibold text-white drop-shadow-lg uppercase">Your Vision, Our Design</h2>
-        <Button href="#about" className="duration-300 font-semibold !shadow-2xl !rounded-none hover:bg-[#000] hover:text-white"
-        sx={{mt: 5, px: 4, py: 1.5, color: "#000", bgcolor: "#bfbebb"}}>
+        <h2 className="text-2xl md:text-4xl font-semibold uppercase text-white drop-shadow-lg">
+          Your Vision, Our Design
+        </h2>
+        <Button
+          href="#about"
+          className="!rounded-none font-semibold !shadow-2xl duration-300 hover:bg-[#000] hover:text-white"
+          sx={{ mt: 5, px: 4, py: 1.5, color: "#000", bgcolor: "#bfbebb" }}
+        >
           Let's Transfrom Your Event
         </Button>
       </div>
 
       {/* Optional dark overlay to improve text readability */}
-      <div className="absolute top-0 left-0 w-full h-full bg-[#A6A5A1] opacity-30 z-0"></div>
+      <div className="absolute left-0 top-0 z-0 h-full w-full bg-[#A6A5A1] opacity-30"></div>
     </section>
   );
 }
